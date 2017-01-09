69c4f055a97a777b0f33424a81689ddc626d4633
Michal Palenik <michal.palenik@freemap.sk>
2017-01-09 13:16:32 +0100

    prehodenie copyright z master stranky do pluginu

---
+++ plugins/copyright.php
@@ -0,0 +1,11 @@
+<?php
+// mozem kontrolovat activediv='#hladaj'
+$config['copyright'] = array(
+	'name' => 'Copyright',
+	// ake je html
+	'menu-horne' => "<i class='fa fa-creative-commons'></i> copyright",
+	'html' => "licencia projektu <a href='#OpenStreetMap'>OpenStreetMap</a> je <a href='#Licencia'>Open Database Licence</a>. data su pod ODbL, tajly pod CC, vrstevnice su z tade, ... navigačné ikonky sú z Font Awesome by Dave Gandy - http://fontawesome.io licencované pod CC-BY <a href='#About'>viac</a>",
+	'css' => '
+'
+);
+?>
