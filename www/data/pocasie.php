e9dc5820df0fd8f052e2d58a3dfd310bf562d858
Michal Palenik <michal.palenik@freemap.sk>
2017-01-09 13:30:00 +0100

    removing .gitignore files

--- www/data/pocasie.php
+++
@@ -1,4 +0,0 @@
-<?php
-if(strlen($_GET["id"]) > 1) echo file_get_contents("http://pocasie.oma.sk/js/pocasie.php?id=".$_GET["id"]);
-else echo file_get_contents("http://pocasie.oma.sk/js/json.php?station_id=".$_GET["station_id"]);
-?>
